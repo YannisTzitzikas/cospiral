60fb536e41c092751bd0bfc3ea85ba1e5635a2c0
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-05-13 15:51:30 +0300

    added jQuery lib

--- WebSite/js/siteScript.js
+++ WebSite/js/siteScript.js
@@ -1,26 +1,3 @@
-/*var slideIndex = [1, 1];
-var slideId = ["slide1", "slide2", "slide3", "slide4"];
-showDivs(1, 0);
-showDivs(1, 1);
-showDivs(1, 2);
-showDivs(1, 3);
-
-
-function plusDivs(n, no) {
-  showDivs((slideIndex[no] += n), no);
-}
-
-function showDivs(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {
-    slideIndex[no] = 1;
-  }
-  if (n < 1) {
-    slideIndex[no] = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no] - 1].style.display = "block";
-}*/ 
+$(document).ready(function () {
+  console.log("Document Loaded Normally");
+});
